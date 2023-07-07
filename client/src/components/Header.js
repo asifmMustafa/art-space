8cbc6085372391cd3cf82dc47f800e9d8cd3bdef
Akib Md Shehabuddin <akib.md.shehabuddin@g.bracu.ac.bd.com>
2023-07-08 01:34:06 +0600

    login functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,39 @@
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../UserContext";
 
 function Header() {
   const navigate = useNavigate();
 
+  const { setId } = useUserContext();
+
+  const logout = () => {
+    setId("");
+    navigate("/");
+  };
+
   return (
     <nav className="bg-gray-50 px-6 py-4 shadow">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-3xl font-bold text-gray-900 flex items-center">
+        <div
+          className="text-3xl font-bold text-gray-900 flex items-center cursor-pointer"
+          onClick={() => {
+            navigate("/browse");
+          }}
+        >
           <span>Art </span>
           <span className="text-blue-600">Space</span>
         </div>
         <div className="flex items-center">
+          <span
+            onClick={() => {
+              navigate("/cart");
+            }}
+            className="text-lg text-blue-500 hover:text-blue-600 cursor-pointer mx-3"
+          >
+            Cart
+          </span>
           <span
             onClick={() => {
               navigate("/favorites");
@@ -29,9 +51,7 @@ function Header() {
             Account
           </span>
           <span
-            onClick={() => {
-              navigate("/logout");
-            }}
+            onClick={logout}
             className="text-lg text-blue-500 hover:text-blue-600 cursor-pointer mx-3"
           >
             Logout
